1602f488d16dbfbe65d49aa4ec9629a20f65b31e
Frumple <frumple1@gmail.com>
2017-12-07 12:37:22 -0800

    Move sample_configs to instance_template directory.
    Remove secret key from default configs. This should instead be set in the config in the instance directory.

--- mrt_file_server/default_config.py
+++ mrt_file_server/default_config.py
@@ -7,9 +7,6 @@ MAX_UPLOAD_FILE_SIZE = 100 * 1024 # 100 kilobytes
 # Maximum number of bytes that can be uploaded at one time
 MAX_CONTENT_LENGTH = MAX_NUMBER_OF_UPLOAD_FILES * MAX_UPLOAD_FILE_SIZE
 
-# Required by Flask to enable sessions and flash messages
-SECRET_KEY = "\x97/\xb6\xecsc\x91\xd3r\xab\xcf\xc0\xd1aez\xc0>\xe2\xf0\xee\xe1\xd5F"
-
 # Enable or disable high-performance file transfer
 # On a Flask development server, this should be set to False as it does not support X_SENDFILE
 # On a test or production server, this should be set to True to ensure the best file transfer speeds
